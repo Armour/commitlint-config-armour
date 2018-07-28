f5db31d2d7486d2726bc87827a33889419e8baa0
Chong Guo <t-choguo@microsoft.com>
2018-07-28 12:59:19 -0700

    Config(commitlint): support upper-case type

--- index.js
+++ index.js
@@ -4,14 +4,10 @@ module.exports = {
     'footer-leading-blank': [2, 'always'],
     'header-max-length': [2, 'always', 100],
     'scope-case': [2, 'always', 'lower-case'],
-    'subject-case': [
-      2,
-      'never',
-      ['sentence-case', 'start-case', 'pascal-case', 'upper-case']
-    ],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
     'subject-empty': [2, 'never'],
     'subject-full-stop': [2, 'never', '.'],
-    'type-case': [2, 'always', 'pascal-case'],
+    'type-case': [2, 'always', ['pascal-case', 'upper-case']],
     'type-empty': [2, 'never'],
     'type-enum': [
       2,
